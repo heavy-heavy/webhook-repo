eaa9a1951f1facbeb9dd5ee0e8b1b142b6a09aed
heavy-heavy <127201347+heavy-heavy@users.noreply.github.com>
2024-09-22 01:20:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    MONGO_URI=os.getenv('MONGO_URI','mongodb+srv://singhprabhav09:8n21EQPCAkFf81pI@cluster0.yychf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    MONGO_URI=os.getenv('MONGO_URI','your_uri')
     PORT=int(os.getenv('PORT',5000))
     DEBUG=os.getenv('DEBUG','TRUE')=='True'
-    SECRET=os.getenv('WEBHOOK_SECRET','heiurnh73vi4btvri3yn4r')
+    SECRET=os.getenv('WEBHOOK_SECRET','your_key')
 
 
     
